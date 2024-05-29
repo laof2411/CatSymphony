365bd94f5bb58ef550da8b1c5155711f4ac2bd7c
arian14 <arian.ma04@.gmail.com>
2024-05-29 09:59:29 -0600

    Json package (simulator), y modificaciones a la escena del menu principal

--- Cat_Symphony/Assets/Scripts/UI SCRIPTS/MainMenuManager.cs
+++ Cat_Symphony/Assets/Scripts/UI SCRIPTS/MainMenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private GameObject creditsScreen;
 
     [SerializeField] private bool menuScreenHasBeenEntered;
+
+    [SerializeField] private GameObject mainBackgroundObj;
+    [SerializeField] private Sprite[] backgroundSprites = new Sprite[5];
+
+
+    [SerializeField] private GameObject menusButtonHolderObj;
 
     //Start is called before the first frame update
     void Start()
@@ -30,6 +37,9 @@ public class MainMenuManager : MonoBehaviour
         HideAllScreens();
         mainMenuScreen.SetActive(true);
 
+        mainBackgroundObj.GetComponent<Image>().sprite = backgroundSprites[0];
+        menusButtonHolderObj.SetActive(false);
+
         menuScreenHasBeenEntered = true;
     }
 
@@ -37,24 +47,36 @@ public class MainMenuManager : MonoBehaviour
     {
         HideAllScreens();
         songSelectionScreen.SetActive(true);
+
+        mainBackgroundObj.GetComponent<Image>().sprite = backgroundSprites[1];
+        menusButtonHolderObj.SetActive(true);
     }
 
     public void OpenDripStoreScreen()
     {
         HideAllScreens();
         dripStoreScreen.SetActive(true);
+
+        mainBackgroundObj.GetComponent<Image>().sprite = backgroundSprites[2];
+        menusButtonHolderObj.SetActive(true);
     }
 
     public void OpenSettingsScreen()
     {
         HideAllScreens();
         settingsScreen.SetActive(true);
+
+        mainBackgroundObj.GetComponent<Image>().sprite = backgroundSprites[3];
+        menusButtonHolderObj.SetActive(true);
     }
 
     public void OpenCreditsScreen()
     {
         HideAllScreens();
         creditsScreen.SetActive(true);
+
+        mainBackgroundObj.GetComponent<Image>().sprite = backgroundSprites[4];
+        menusButtonHolderObj.SetActive(false);
     }
 
     public void HideAllScreens()

a5696fb947b9dbcacf5621b81cd6ad6e638c2fab
laof2411 <laof2411@gmail.com>
2024-06-04 12:43:56 -0600

    UWU

---
+++ Cat_Symphony/Assets/Scripts/Manager/NoteTouchManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NoteTouchManager : MonoBehaviour
+{
+
+
+    [SerializeField] private TextMeshProUGUI UItext;
+
+    [SerializeField] private float missDistance;
+    [SerializeField] private float okayDistance;
+    [SerializeField] private float greatDistance;
+    [SerializeField] private float purrfectDistance;
+
+}
--- Cat_Symphony/Assets/Scripts/NoteScripts/SingleTapNoteEvent.cs
+++ Cat_Symphony/Assets/Scripts/NoteScripts/SingleTapNoteEvent.cs
@@ -1,27 +1,56 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
-public class SingleTapNoteEvent : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
+public class SingleTapNoteEvent : MonoBehaviour, IPointerClickHandler
 {
-    
+
+    [SerializeField] private Transform transformObjective;
+    [SerializeField] private TextMeshProUGUI UItext;
+
+    [SerializeField] private float missDistance;
+    [SerializeField] private float okayDistance;
+    [SerializeField] private float greatDistance;
+    [SerializeField] private float purrfectDistance;
+
     public void OnPointerClick(PointerEventData eventData)
     {
 
         SingleTap();
 
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-
-        SingleTap();
-
-    }
+    }    
 
     private void SingleTap()
     {
 
-        Destroy(gameObject);
+        if(Vector3.Distance(transformObjective.position, transform.position) <= purrfectDistance)
+        {
+
+            UItext.text = "Purrfect";
+            Destroy(gameObject);
+
+        }
+        else if(Vector3.Distance(transformObjective.position, transform.position) <= greatDistance)
+        {
+
+            UItext.text = "Great";
+            Destroy(gameObject);
+
+        }
+        else if (Vector3.Distance(transformObjective.position, transform.position) <= okayDistance)
+        {
+
+            UItext.text = "Okay";
+            Destroy(gameObject);
+
+        }
+        else
+        {
+
+            UItext.text = "Miss";
+            Destroy(gameObject);
+
+        }
 
     }
 

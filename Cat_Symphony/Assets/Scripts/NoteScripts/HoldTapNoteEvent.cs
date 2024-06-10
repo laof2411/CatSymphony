cd6b4ed3d2f02469905c7dd0183e62434d11ad4f
laof2411 <laof2411@gmail.com>
2024-06-10 11:00:03 -0600

    tuu

--- Cat_Symphony/Assets/Scripts/NoteScripts/HoldTapNoteEvent.cs
+++ Cat_Symphony/Assets/Scripts/NoteScripts/HoldTapNoteEvent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HoldTapNote : MonoBehaviour
+public class HoldTapNoteEvent : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

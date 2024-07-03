04a15827e97104f39eafe865adc03b8f604026bf
MAGI-PERSONAL <magicogumelo@gmail.com>
2024-07-02 21:57:32 -0600

    Spawner

---
+++ Cat_Symphony/Assets/Scripts/Manager/NoteSpawnerManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSpawnerManager : MonoBehaviour
+{
+
+    [Header("Note Prefabs")]
+    [SerializeField] private GameObject singleTapNote;
+    [SerializeField] private GameObject holdTapNote;
+    [SerializeField] private GameObject freeStyleNote;
+
+
+
+}

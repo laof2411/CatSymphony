5d0db24e77e89dea53ba4e82d72a68fb430a52d1
MAGI-PERSONAL <magicogumelo@gmail.com>
2024-07-03 19:07:59 -0600

    Adiciones a el spawneo de notas

--- Cat_Symphony/Assets/Scripts/Manager/NoteSpawnerManager.cs
+++ Cat_Symphony/Assets/Scripts/Manager/NoteSpawnerManager.cs
@@ -10,6 +10,35 @@ public class NoteSpawnerManager : MonoBehaviour
     [SerializeField] private GameObject holdTapNote;
     [SerializeField] private GameObject freeStyleNote;
 
+    [SerializeField] private GameObject[] spawnPoints;
+    [SerializeField] private GameObject[] objectives;
 
+    [SerializeField] private NoteInformation[] notesToSpawn;
+
+    private void Update()
+    {
+
+
+
+    }
+
+}
+
+
+public struct NoteInformation
+{
+
+    public NoteType type;
+    public int number_SpawnPoint;
+    public int number_objective;
+
+    public float timeToReachPerfect;
+
+}
+
+public enum NoteType
+{
+
+    SingleTap,HoldTap,FreeStyle
 
 }

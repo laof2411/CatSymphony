1192426eb05e79dff0a75d8f1881ed404a7a1cd3
arian14 <arian.ma04@.gmail.com>
2024-07-09 11:56:58 -0600

    Niveles test

---
+++ Cat_Symphony/Assets/Scripts/LevelScripts/LevelPoints.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPoints : MonoBehaviour
+{
+    [SerializeField] public int accumulatedPoints;
+    [SerializeField] public int accumulatedStars;
+
+}

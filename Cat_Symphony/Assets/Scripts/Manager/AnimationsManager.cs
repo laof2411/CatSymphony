b2ef3412822e2a83757139f344890aaafd5c520c
MAGI-PERSONAL <magicogumelo@gmail.com>
2024-08-01 12:26:44 -0600

    Ya tiene animaciones el gato

--- Cat_Symphony/Assets/Scripts/Manager/AnimationsManager.cs
+++ Cat_Symphony/Assets/Scripts/Manager/AnimationsManager.cs
@@ -7,11 +7,42 @@ public class AnimationsManager : MonoBehaviour
 
     // 0 - blue 1 - green 2 - yellow 3 - red
     [SerializeField] Animator[] bongosAnimators;
+    [SerializeField] Animator catAnimator;
 
     public void TapBongo(int trailNumber)
     {
 
         bongosAnimators[trailNumber - 1].Play("Tap");
+        
+        switch (trailNumber)
+        {
+
+            case 1:
+                {
+
+                    catAnimator.Play("Bong Blue");
+                    break;
+                }
+            case 2:
+                {
+
+                    catAnimator.Play("Bong Green");
+                    break;
+                }
+            case 3:
+                {
+
+                    catAnimator.Play("Bong Yellow");
+                    break;
+                }
+            case 4:
+                {
+
+                    catAnimator.Play("Bong Red");
+                    break;
+                }
+
+        }
 
     }
 
